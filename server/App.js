1a9b3e2f0e72f9a1ffd021c34031ba534614da69
Aslihan Aria Celik <aslihancelik27@gmail.com>
2025-02-26 21:01:40 -0500

    finished setting up the server

--- server/App.js
+++ server/App.js
@@ -1,4 +1,4 @@
-//only command to run after npm i
+//only command to run after getting all this to your branch is: npm i , and make sure you run this while you are in server folder
 
 // const express = require("express");
 // const mongoose = require("mongoose");
