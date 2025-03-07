a07f0a818952b3ad732ac09e22f014cf07344a89
Aslihan Aria Celik <aslihancelik27@gmail.com>
2025-03-06 21:03:09 -0500

    added account_models.js code , account schema

--- server/models/account_models.js
+++ server/models/account_models.js
@@ -1,1 +1,56 @@
 const mongoose = require("mongoose");
+
+const AccountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, 
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    creditCard: {
+      number: {
+        type: String,
+        required: function () {
+          return this.type === "credit card";
+        },
+      },
+      expDate: {
+        type: String,
+        required: function () {
+          return this.type === "credit card";
+        },
+      },
+      cvc: {
+        type: String,
+        required: function () {
+          return this.type === "credit card";
+        },
+      },
+    },
+    bankAccount: {
+      routingNumber: {
+        type: String,
+        required: function () {
+          return this.type === "bank account";
+        },
+      },
+      accountNumber: {
+        type: String,
+        required: function () {
+          return this.type === "bank account";
+        },
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+module.export = mongoose.model("Account", AccountSchema);
