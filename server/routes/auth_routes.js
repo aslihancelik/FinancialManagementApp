a1e8507577158b75cee5c2d86b44e7b93e0ce53a
sondosassallami <sondos_fadle@yahoo.com>
2025-03-20 19:34:53 -0400

    bills auth work

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -4,8 +4,8 @@ const { signup, login, logout, updateUser }  = require("../controllers/auth_cont
 
 //console.log("Auth routes registered");  // Add this line
 
-router.post("/signup", signup); //localhost:3000/auth/signup 
-router.post("/login", login); //localhost:3000/auth/login
+router.post("/signup", signup); //http://localhost:3000/auth/signup make sure Auth Bearer token is blank, no "," after password
+router.post("/login", login); //localhost:3000/auth/login to get a token
 router.post("/logout", logout); //localhost:3000/auth/logout
 router.put("/update/:userId", updateUser); //localhost:3000/auth/update/:userId
 
